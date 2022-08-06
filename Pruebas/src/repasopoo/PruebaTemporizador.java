4e1a5d96294d103169823a977adff73e9b0e5ab3
Nikken <nicolasromanblanco@gmail.com>
2022-08-06 19:46:50 +0200

    Uso de clases internas

--- Pruebas/src/repasopoo/PruebaTemporizador.java
+++ Pruebas/src/repasopoo/PruebaTemporizador.java
@@ -1,5 +1,6 @@
 package repasopoo;
 
+import java.awt.Toolkit;
 import java.awt.event.*;
 import java.util.Date;
 import javax.swing.JOptionPane;
@@ -31,11 +32,16 @@ public class PruebaTemporizador {
 
 class DameLaHora implements ActionListener{
 	
+	
+	
 	public void actionPerformed(ActionEvent e) {
 		
 		Date ahora=new Date();
 		
 		System.out.println("Pongo la hora cada 5 segundos: "+ahora);
+		
+		//Toolkit: Utiliza recursos del sistema operativo en nuestros programas java (mira https://docs.oracle.com/javase/7/docs/api/ para mas info)
+		Toolkit.getDefaultToolkit().beep();
 		
 	}
 	
